18f7d8f471e5ed7d1317491abdac13d2d0eb25b7
webpwnized <webpwnized@gmail.com>
2024-01-21 17:26:50 -0500

    1.0.1 Update scripts

--- scripts/update-dragonfly-application.sh
+++ scripts/update-dragonfly-application.sh
@@ -16,4 +16,4 @@ echo "Updating the application installed in the running 'www' container.";
 
 # Use 'docker exec' to execute commands inside the 'www' container.
 # First, ensure Git is installed by running 'apt install git -y' within the container.
-docker exec -it www sh -c "apt update; apt install git -y; cd /var/www/dragonfly; git pull"
+docker exec -it  -u root www /bin/bash -c "apt update; apt install git -y; cd /var/www/dragonfly; git pull; echo \"Application Version: \$(cat version)\";"
